7745126af1cab4a24712114ed4c4b914f3c5253a
THiddle03 <250194633+THiddle03@users.noreply.github.com>
2026-04-11 22:01:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -142,32 +142,13 @@ if st.sidebar.button("Simular Proceso", type="primary"):
   
 # ... (Todo el código anterior de simulación y lógica se mantiene igual)
 
-# ... (Todo el código anterior de simulación se mantiene igual)
-
 # MOSTRAR RESULTADOS
 if 'resultados' in st.session_state:
     dm, de, ec, pf = st.session_state['resultados']
     
     if pf and os.path.exists(pf):
         st.image(pf, caption="PFD del Sistema")
-        
-        # --- NUEVA FORMA NATIVA DE AGREGAR LINKS ---
-        st.write("### 📂 Documentación ISO")
-        col_btn1, col_btn2 = st.columns(2)
-        
-        with col_btn1:
-            st.link_button(
-                "DB ISO 📄", 
-                "https://drive.google.com/file/d/1-zug-EVSwEHDY9qQMyisWeOu00l7RBiE/view?usp=drive_link",
-                use_container_width=True
-            )
-
-        with col_btn2:
-            st.link_button(
-                "DFP ISO 📄", 
-                "https://drive.google.com/file/d/1gK13k1si35ynEmnBhKy7bKEF_xhVdP_b/view?usp=drive_link",
-                use_container_width=True
-            )
+     
         # ------------------------------------------
 
     col1, col2 = st.columns(2)
